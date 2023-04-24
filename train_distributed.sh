119bb0bd5d2b14f390cca5c6d8f4ec8a27d460c4
EricLipe17 <EricLipe17@gmail.com>
2023-04-24 07:21:22 -0600

    - updated defaults

--- train_distributed.sh
+++ train_distributed.sh
@@ -10,7 +10,7 @@ main_dist.py"
 # Start master node
 torchrun \
 --nproc_per_node=1 --nnodes=4 --node_rank=0 \
---master_addr=104.171.200.62 --master_port=38017 \
+--master_addr=129.82.44.124 --master_port=38017 \
 main_dist.py
 
 # Start worker nodes
